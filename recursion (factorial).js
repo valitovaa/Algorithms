486a4e041692f7f0370851735d84dd2437b81409
Dilara Valitova <87713511+valitovaa@users.noreply.github.com>
2022-03-22 11:34:04 +0500

    Update recursion (factorial).js

--- recursion (factorial).js
+++ recursion (factorial).js
@@ -1,3 +1,4 @@
+1)
 function fact(x){
     if(x===1){
         return 1
@@ -6,3 +7,23 @@ function fact(x){
     }
 }
 console.log(fact(3))
+2)
+function sum(arr){
+    let total = 0
+    for(i in arr){
+        total+=arr[i]
+    }
+    return total
+}
+//recursion
+
+function recSum(arr){
+    if(arr === []){
+        return arr
+    }else{
+        let i = arr.shift()
+        return i + sum(arr)
+    }
+}
+let a = [2,2,3,8]
+console.log(recSum(a))
